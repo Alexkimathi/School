bd746e17b6e1b497e6e7b24a4f1e74b2b7cef07c
AlexKimathi <keemalekiegmail.com>
2023-02-13 10:32:48 +0300

    adds facebook icons

--- frontend/src/components/footer/data.js
+++ frontend/src/components/footer/data.js
@@ -1,13 +1,13 @@
 import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
-// import {FaFacebookSquare} from 'react-icons/fa'
+import {AiFillFacebook} from 'react-icons/ai'
 
 
 
 const data = [
     {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    // {id: 3, link: 'https://facebook.com', icon: <FaFacebookSquare/>},
+    {id: 3, link: 'https://facebook.com', icon: <AiFillFacebook/>},
   
 ]
 export default data
